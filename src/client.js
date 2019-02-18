5d717fe477e5557a614b2d616e843966b437bbb3
qysp <qysp.yo@gmail.com>
2019-02-18 16:55:08 +0100

    extend client functions

--- src/client.js
+++ src/client.js
@@ -1,7 +1,6 @@
-/**
- * Based on and inspired by NexusNull:
- * https://github.com/NexusNull/ALBot/blob/master/httpWrapper.js
- */
+// Based on and inspired by NexusNull:
+// https://github.com/NexusNull/ALBot/blob/master/httpWrapper.js
+
 const request = require('request-promise-native');
 const { handleError, } = require('./helpers');
 
@@ -16,6 +15,7 @@ const AdventureLandClient = function(config) {
   this.url = `${config.https ? 'https' : 'http'}://adventure.land`;
   this.email = config.email;
   this.password = config.password;
+  this.loggedIn = false;
 };
 
 /**
@@ -29,26 +29,24 @@ AdventureLandClient.prototype.login = async function() {
   }
 
   const headers = {
+    Accept: 'application/json, text/javascript, */*; q=0.01',
     'X-Requested-With': 'XMLHttpRequest',
     'User-Agent': 'AdventureLandTerminalClient: (v1.0.0)',
-    'Accept': 'application/json, text/javascript, */*; q=0.01',
     'Content-Type': 'application/x-www-form-urlencoded',
     'Cache-Control': 'no-cache',
   };
 
-  const options = {
-    uri: `${this.url}/api/signup_or_login`,
-    form: form,
-    headers: headers,
-    resolveWithFullResponse: true
-  };
-
   await request
-    .post(options)
+    .post({
+      uri: `${this.url}/api/signup_or_login`,
+      form: form,
+      headers: headers,
+      resolveWithFullResponse: true,
+    })
     .then(res => {
       const success = JSON.parse(res.body).find(elem => elem.message === 'Logged In!') !== undefined;
       if (!success) {
-        throw new Error('Error: Login failed');
+        throw new Error('Failed to login');
       }
       // set session (auth) cookie and user id
       this.sessionCookie = res.headers['set-cookie']
@@ -56,9 +54,74 @@ AdventureLandClient.prototype.login = async function() {
         .split(';')[0]
         .split('=')[1];
       this.userId = this.sessionCookie.split('-')[0];
+      this.loggedIn = true;
     })
     .catch(err => handleError(err.message, true));
 }
 
+/**
+ * Fetch the characters of the logged in account.
+ * @returns an array of objects where each object represents a character
+ */
+AdventureLandClient.prototype.getCharacters = async function() {
+  if (!this.loggedIn) {
+    throw new Error('Must login before fetching characters');
+  }
+
+  const form = { method: 'servers_and_characters', };
+  const headers = { Cookie: `auth=${this.sessionCookie}`, };
+
+  return await request
+    .post({
+      uri: `${this.url}/api/servers_and_characters`,
+      form: form,
+      headers: headers,
+    })
+    .then(body => {
+      const jsonData = JSON.parse(body)[0];
+      // only the id and name of each character is needed
+      return jsonData.characters
+        .map(c => ({ id: c.id, name: c.name }));
+    })
+    .catch(err => handleError(err.message, true));
+}
+
+/**
+ * Fetch the available servers.
+ * @returns an array of objects where each objects represents a server
+ */
+AdventureLandClient.prototype.getServers = async function() {
+  if (!this.loggedIn) {
+    throw new Error('Must login before fetching servers');
+  }
+
+  const form = { method: 'get_servers', };
+  const headers = { Cookie: `auth=${this.sessionCookie}`, };
+
+  return await request
+    .post({
+      uri: `${this.url}/api/get_servers`,
+      form: form,
+      headers: headers,
+    })
+    .then(body => {
+      const jsonData = JSON.parse(body)[0];
+      if (jsonData.type !== 'success') {
+        throw new Error('Failed fetching server list');
+      }
+      return jsonData.message;
+    })
+    .catch(err => handleError(err.message, true));
+}
+
+/**
+ * Load a file from the public Adventure Land website.
+ * @param {String} filepath name of the file's path
+ */
+AdventureLandClient.prototype.getGameFile = async function(filepath) {
+  return await request
+    .get(`${this.url}/${filepath}`)
+    .catch(err => handleError(err.message));
+}
 
 module.exports = AdventureLandClient;
