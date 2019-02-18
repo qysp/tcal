16cc20ba7c66d879cbc012ea832fbe5593bb5899
qysp <qysp.yo@gmail.com>
2019-02-18 16:56:54 +0100

    load necessary files and store them

--- README.md
+++ README.md
@@ -13,3 +13,8 @@
 2. rename `config.example.js` to `config.js`
 3. run `npm install` to install the necessary dependencies
 4. finally, run `node index.js` to start the client
+
+## Start parameters
+Note: if a necessary file does not exist, it will be loaded even if no corresponding paramater is set.
+* --fetch: fetch your characters and the available servers even if they already exist in your `data` folder.
+* --nocache: load all game files from the server even if they already exist in your `game_files` folder.
--- index.js
+++ index.js
@@ -1,18 +1,57 @@
+const fs = require('fs');
 const path = require('path');
+const argv = require('minimist')(process.argv.slice(2));
+
 const AdventureLandClient = require('./src/client');
 const { tryTo, handleError, } = require('./src/helpers');
+
 
 async function run() {
   // try to require the necessary config file
   const config = await tryTo(require, path.join(__dirname, 'config'))
-    .then(ret => ret)
-    .catch(err => handleError(err, true));
+    .catch(err => handleError(err.message, true));
 
   // init Adventure Land client
   const client = new AdventureLandClient(config);
 
   // async login
   await client.login();
+
+  // fetch characters if needed/requested
+  const charactersPath = path.join(__dirname, 'data', 'characters.json');
+  if (argv.fetch || !fs.existsSync(charactersPath)) {
+    const characters = await client.getCharacters();
+    fs.writeFileSync(charactersPath, JSON.stringify(characters, null, 4));
+  }
+
+  // fetch servers if needed/requested
+  const serversPath = path.join(__dirname, 'data', 'servers.json');
+  if (argv.fetch || !fs.existsSync(serversPath)) {
+    const servers = await client.getServers();
+    fs.writeFileSync(serversPath, JSON.stringify(servers, null, 4));
+  }
+
+  // all game files that are needed
+  const gameFiles = [
+    'data.js',
+    'js/game.js',
+    'js/functions.js',
+    'js/common_functions.js',
+    'js/runner_functions.js',
+  ];
+
+  for (let gameFile of gameFiles) {
+    const gameFilePath = path.join(
+      __dirname,
+      'game_files',
+      gameFile.replace('js/', ''));
+
+    if (argv.nocache || !fs.existsSync(gameFilePath)) {
+      const data = await client.getGameFile(gameFile);
+      if (!data) continue;
+      fs.writeFileSync(gameFilePath, data);
+    }
+  }
 }
 
 run();
