8a772187547f50ac68f5d7dd30a8528a25cdf434
qysp <43863063+qysp@users.noreply.github.com>
2019-02-23 17:13:08 +0100

    general: cleanup

--- src/emulator.js
+++ src/emulator.js
@@ -19,9 +19,6 @@ const { JSDOM } = jsdom;
     runScripts: 'dangerously',
     resources: 'usable',
     pretendToBeVisual: true,
-    beforeParse: window => {
-      window.eval('var requestAnimationFrame = window.requestAnimationFrame;')
-    },
   })).window;
 
   // TODO: could you realiably overwrite the server_addr / server_port before the socket is initialized?
@@ -29,13 +26,12 @@ const { JSDOM } = jsdom;
   // await the resource loading (initiaization of necessary window properties)
   await new Promise(resolve => setInterval(() => window.socket && resolve(), 100));
 
-  // shorter version
+  // shorter version for log_in
   const login = () => window.log_in(window.user_id, args.characterId, window.user_auth);
 
   // game loaded, ready to login
   window.socket.on('welcome', () => {
     login();
-    console.log('[JSDOM]: Logging in...');
   });
 
   // unsuccessful login
@@ -44,13 +40,12 @@ const { JSDOM } = jsdom;
     if (err === 'Failed: ingame') {
       setTimeout(() => login(), 35000);
     } else if ((match = err.match(/wait_(\d+)_seconds/))) {
-      setTimeout(() => login(), match[1])
+      setTimeout(() => login(), parseInt(match[1]));
     }
   });
 
   // successful login
   window.socket.on('start', () => {
-    console.log(`[JSDOM]: Character loaded '${window.character.name}'`);
     if (args.script) {
       window.start_runner(undefined, args.script);
     }
